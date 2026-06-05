0880df07376f2767f9faa69ded2fc1f9b1822470
Matteo Chauvin <mchauvin@z2r10p6.42lyon.fr>
2026-06-05 11:14:44 +0200

    refactor(class): create Metadata class instead of mixing them

--- src/mapdata.py
+++ src/mapdata.py
@@ -6,25 +6,31 @@ class ParsingError(Exception):
     pass
 
 
-@dataclass
+@dataclass(frozen=True)
+class Metadata():
+    zone_type: str = "normal"
+    color: Optional[str] = None
+    max_drones: int = 1
+    max_capacity: int = 1
+
+
+@dataclass(frozen=True)
 class Zone():
     name: str
     x: int
     y: int
-    zone_type: str = "normal"
-    color: Optional[str] = None
-    max_drones: int = 1
+    metadata: Metadata
 
     def __post_init__(self) -> None:
         allowed_types = ["normal", "blocked", "restricted", "priority"]
-        if self.zone_type not in allowed_types:
+        if self.metadata.zone_type not in allowed_types:
             raise ParsingError(f"Invalid type zone: {self.zone_type}")
-        if self.max_drones <= 0:
+        if self.metadata.max_drones <= 0:
             raise ParsingError("max_drone needs to be a positive integer.")
 
 
-@dataclass
+@dataclass(frozen=True)
 class Connection:
     source: str
     destination: str
-    max_capacity: int = 1
+    metadata: Metadata

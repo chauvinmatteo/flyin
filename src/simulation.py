62acb4c9646fd5737e9b496cadcd59e0138b2735
Matteo Chauvin <mchauvin@z4r6p6.42lyon.fr>
2026-07-07 14:06:17 +0200

    dev(algo): begginning of a* with best move calculator function

--- src/simulation.py
+++ src/simulation.py
@@ -17,6 +17,9 @@ class Simulation():
             self.graph[conn.destination].append(conn.source)
         self.goal = [name for name, z in zone.items()
                      if z.role == "end_hub"][0]
+        self.drones_in_zone: dict[str, list[str]] = {name: [] for
+                                                     name in self.graph}
+        self.dist_map = self.compute_dijkstra_costs(self.goal)
         self.drones_path: dict[int, list[str]] = {}
 
     def _get_weight(self, zone_name: str) -> float:
@@ -46,8 +49,27 @@ class Simulation():
                     queue.append(neighbor)
         return distances
 
+    def get_best_move(self, drone_current_zone: str) -> str:
+        best_score = float('inf')
+        best_move = None
+        for neighbor in self.graph[drone_current_zone]:
+            zone_data = self.zone[neighbor]
+            nb_drone = len(self.drones_in_zone[neighbor])
+            max_drones = zone_data.metadata.max_drones
+            new_cost = (nb_drone / max_drones) * 5.0
+            g = self._get_weight(neighbor) + new_cost
+            h = self.dist_map.get(neighbor, float('inf'))
+            f = g + h
+            if f < best_score:
+                best_score = f
+                best_move = neighbor
+        return best_move if best_move is not None else drone_current_zone
+
     def print_graph(self) -> None:
         for zone, voisins in self.graph.items():
             print(f"{zone} is link to : {', '.join(voisins)}")
+        for zone in self.graph:
+            best = self.get_best_move(zone)
+            print(f"Depuis {zone}, le drone choisit d'aller vers : {best}")
         print(f"the goal is : {self.goal}")
         print(self.compute_dijkstra_costs(self.goal))

3fa9c9287d1d249c5535233f941ffb35fb0e6352
Matteo Chauvin <mchauvin@z3r13p1.42lyon.fr>
2026-08-21 10:50:58 +0200

    dev(main): main to test with Makefile

--- src/fly_in.py
+++ src/fly_in.py
@@ -1,27 +1,12 @@
-from mapdata import ParsingError
-from simulation import Simulation
-from parsing import MapParser
+import arcade
+from visualizer import MapSelectionView
 
 
 def main() -> None:
-    maps = [
-            "./maps/easy/01_linear_path.txt"
-            ]
-    parser = MapParser()
-    try:
-        for map in maps:
-            parser.parse(map)
-            simulation = Simulation(
-                zone=parser.zones,
-                connection=parser.connections,
-                drone_nb=parser.drones_nb
-            )
-            # simulation.print_graph()
-            while not simulation.all_drones_arrived():
-                simulation.step()
-                print(simulation.turn)
-    except ParsingError as e:
-        print(f"ParsingError: {e}")
+    window = arcade.Window(1200, 800, "Fly In - Visualiseur")
+    menu_depart = MapSelectionView()
+    window.show_view(menu_depart)
+    arcade.run()
 
 
 if __name__ == "__main__":
